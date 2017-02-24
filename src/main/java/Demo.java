4288ecc5cabcfa1149b72df8f2e12931819db3e4
Dakshina Murthy Malladi <dakshinamurthy.malladi@factset.com>
2017-02-24 15:33:31 +0530

    spark demo.

---
+++ src/main/java/Demo.java
@@ -0,0 +1,26 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+
+
+public class Demo {
+    public static void main(String[] args) {
+        String logFile = "C:\\spark-2.0.2-bin-hadoop2.7\\README.md"; // Should be some file on your system
+        SparkConf conf = new SparkConf().setAppName("Simple Application").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaRDD<String> logData = sc.textFile(logFile).cache();
+
+        long numAs = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) { return s.contains("a"); }
+        }).count();
+
+        long numBs = logData.filter(new Function<String, Boolean>() {
+            public Boolean call(String s) { return s.contains("b"); }
+        }).count();
+
+        System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+        sc.stop();
+    }
+}
